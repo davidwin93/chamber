03e72b6878ee884c3d4723c1f7a3c807fc2ad333
Dave Winiarski <david@adpt.ca>
2024-06-10 20:26:32 -0600

    add new tests and default config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,17 +1,45 @@
 package config
 
+import (
+	"net"
+	"sync"
+)
+
 type Config struct {
 	FirecrackerBinaryPath string
+	GatewayIP             net.IP
+	SubnetMask            string
 }
 
-func LoadConfig() (*Config, error) {
-	return &Config{
-		FirecrackerBinaryPath: "/usr/local/bin/firecracker",
-	}, nil
+var config *Config
+var lock sync.RWMutex
+
+func GetConfig() *Config {
+	lock.RLock()
+	if config != nil {
+		defer lock.RUnlock()
+		return config
+	}
+	lock.RUnlock()
+
+	lock.Lock()
+	defer lock.Unlock()
+	if config != nil {
+		return config
+	}
+	config = loadConfig()
+	return config
+}
+func loadConfig() *Config {
+	//Check well known locations and env if not return default
+	//cache config to prevent expensive reload
+	return LoadDefaultConfig()
 }
 
 func LoadDefaultConfig() *Config {
 	return &Config{
 		FirecrackerBinaryPath: "./vm/firecracker",
+		GatewayIP:             net.ParseIP("172.102.0.1"),
+		SubnetMask:            "255.255.0.0",
 	}
 }
--- test.sh
+++ test.sh
@@ -1,4 +1,14 @@
 curl --header "Content-Type: application/json" \
   --request POST \
   --data '{"name":"nginx","image":"nginx","dstPort":"80","protocol":"tcp"}' \
-  http://localhost:8070/create
+  http://localhost:8070/vm
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"name":"go-httpbin","image":"mccutchen/go-httpbin","dstPort":"8080","protocol":"tcp"}' \
+  http://localhost:8070/vm
+
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"name":"httpbin","image":"kennethreitz/httpbin","dstPort":"80","protocol":"tcp"}' \
+  http://localhost:8070/vm
